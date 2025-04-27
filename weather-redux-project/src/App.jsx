8920718f9481ac4b9fcdc65dbecedd0e41352f4a
Francisco C <ingefran57@gmail.com>
2025-04-27 17:03:04 -0300

    nuevo color de p en app.jsx

--- weather-redux-project/src/App.jsx
+++ weather-redux-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <p style={{color: 'ligthblue' }}  > The API source: " api.openweathermap.org/data/2.5/weather?q=t,AR& " </p>
+      <p style={{color:"cornflowerblue" }}  > The API source: " api.openweathermap.org/data/2.5/weather?q=t,AR& " </p>
       <br></br>
       <TheProvider>
         <UserConsumer></UserConsumer>
